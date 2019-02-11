b0606406ad46e7f66ec761cf57c172e2eaec9ceb
Rajendra Bhagroo <rajendra@inspiron.fios-router.home>
2019-02-11 04:09:23 -0500

    Refactored Server Code Into Its Own Folder For Clarity. Setup Scripts. Updated README. Signed.
    Signed-off-by: Rajendra Bhagroo <RajendraBhagroo@Gmail.com>

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Database Configuration
-const db = require("./config/keys").mongoURI;
+const db = require("../config/keys").mongoURI;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -18,6 +18,10 @@ mongoose
 
 // Run Server
 const host = "localhost";
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server running on http://${host}:${port}`));
+
+app.get("/", (req, res) => {
+  res.status(200).json("TeleQuest Server Alive");
+});
