b6296fc187a1be15412146a8470b18dbb4e79f69
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-02-17 01:41:34 -0500

    Unit Testing Integrated, Common HTTP Methods [GET,POST,DELETE] Included For [Register, Login, Current, Delete] Routes. JWT Bearer Token Works

---
+++ server/tests/users.test.js
@@ -0,0 +1,56 @@
+const config = require("../../config/config.development");
+const request = require("supertest").agent(
+  `http://${config.host}:${config.node_port}`
+);
+
+/*
+ * @route   GET api/v1/users/test
+ * @desc    Tests users route
+ * @access  Public
+ */
+describe("GET /api/v1/users/", () => {
+  it("Should Return JSON Message", done => {
+    request
+      .get("/api/v1/users/")
+      .set("Accept", "application/json")
+      .expect(
+        200,
+        {
+          msg: "User Test Route"
+        },
+        done
+      );
+  });
+});
+
+/*
+ * @route   POST api/v1/users/register
+ * @params  {name, email, password}
+ * @desc    Register user
+ * @access  Public
+ */
+describe("POST /api/v1/users/register", () => {
+  const body = {
+    name: "John Doe",
+    email: "JohnDoe@Gmail.com",
+    password: "123456"
+  };
+  it("Should Respond With 201 Created", done => {
+    request
+      .post("/api/v1/users/register")
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+// Test -> Register Missing 1 Param SHOULD SEND 400
+// Test -> Register with existing email SHOULD SEND 400
+// Test -> User Login: Success
+// Test -> User Login: Fail Incorrect Password SEND 400
+// Test -> User Delete: Success
+// Test -> User Delete: ID Not Found SEND 400
