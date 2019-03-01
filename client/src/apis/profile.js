d8ed7bbda7628aa9e8e86f951aa9f6edd1762f26
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-03-01 06:24:52 -0500

    Refactored .env To Work With Client And Refactored Version Numbers For Server Routes/Testing For Re-Usability

--- client/src/apis/profile.js
+++ client/src/apis/profile.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/api/v1/profile"
+  baseURL: `http://localhost:3001/api/${process.env.ROUTE_VERSION}/profile`
 });
