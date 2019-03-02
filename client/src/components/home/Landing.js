7c0dc00d8ce6d48e44e186bb94346c0078687718
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-03-02 08:24:42 -0500

    Navbar, Footer Date, About section Implemented. Basic Routing Functional

--- client/src/components/home/Landing.js
+++ client/src/components/home/Landing.js
@@ -7,10 +7,6 @@ class Landing extends React.Component {
     return (
       <div>
         <h1>Landing</h1>
-        <img
-          src={require("../resources/images/TeleQuest_Logo.png")}
-          alt="TeleQuest Logo"
-        />
       </div>
     );
   }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../resources/images/TeleQuest_Logo.png";
+
+const styles = {
+  Logo: {
+    width: "100px",
+    height: "30px"
+  }
+};
 
 class Navbar extends React.Component {
   state = {};
 
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+        <Link className="navbar-brand mr-4" to="/">
+          <img style={styles.Logo} src={Logo} alt="TeleQuest Logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggle"
+          aria-controls="navbarToggle"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarToggle">
+          <div className="navbar-nav mr-auto">
+            <Link className="nav-item nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-item nav-link" to="/about">
+              About
+            </Link>
+          </div>
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link mr-2" to="/login">
+              Login
+              <i className="fas fa-sign-in-alt ml-2" />
+            </Link>
+            <Link className="nav-item nav-link" to="/register">
+              Register
+              <i className="fas fa-registered ml-2" />
+            </Link>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
