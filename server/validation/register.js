60cdd2db1ff3cc2b89622a8c0e1e08483c1ba87a
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-03-09 05:58:13 -0500

    Clever Way To Combat NoSQL Injection Through String Coercion & Refactored Validation Checking Of Users Login/Register. Data Is Validated, Centralized & Sanitized

--- server/validation/register.js
+++ server/validation/register.js
@@ -3,46 +3,56 @@ const isEmpty = require("./is-empty");
 
 module.exports = validateRegisterInput = data => {
   let errors = {};
+  let body = {};
 
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  body.name = !isEmpty(data.name) ? data.name.trim() : "";
+  body.email = !isEmpty(data.email) ? data.email.trim().toLowerCase() : "";
+  body.password = !isEmpty(data.password) ? data.password.trim() : "";
+  body.password2 = !isEmpty(data.password2) ? data.password2.trim() : "";
 
-  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+  if (!Validator.isLength(body.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
 
-  if (Validator.isEmpty(data.name)) {
+  if (!Validator.isAlpha(body.name)) {
+    errors.name = "Name must only contain letters";
+  }
+
+  if (Validator.isEmpty(body.name)) {
     errors.name = "Name field is required";
   }
 
-  if (Validator.isEmpty(data.email)) {
+  if (Validator.isEmpty(body.email)) {
     errors.email = "Email field is required";
   }
 
-  if (!Validator.isEmail(data.email)) {
+  if (!Validator.isEmail(body.email)) {
     errors.email = "Email is invalid";
   }
 
-  if (Validator.isEmpty(data.password)) {
+  if (Validator.isEmpty(body.password)) {
     errors.password = "Password field is required";
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+  if (!Validator.contains(body.email, "@nyit.edu")) {
+    errors.email = "Educational email is required [@nyit.edu]";
+  }
+
+  if (!Validator.isLength(body.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
   }
 
-  if (Validator.isEmpty(data.password2)) {
+  if (Validator.isEmpty(body.password2)) {
     errors.password2 = "Confirm Password field is required";
   } else {
-    if (!Validator.equals(data.password, data.password2)) {
+    if (!Validator.equals(body.password, body.password2)) {
       errors.password2 = "Passwords must match";
     }
   }
 
   return {
     errors,
+    body,
     isValid: isEmpty(errors)
   };
 };
