994ce1102f17f0ac09d6e7db1ff07c59078cd182
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-03-19 03:47:33 -0400

    Profile Test Routes Completed. Next Steps: Remove Requirements From Profile Model And Enforce Them In Validation Files. All While Still Passing Tests [Update Profile Tests To Reflect Cases In Profile Validation]

--- server/v1/routes/api/profile.js
+++ server/v1/routes/api/profile.js
@@ -10,8 +10,8 @@ const User = require("../../models/User");
 const validateProfileInput = require("../../validation/profile/profile");
 const validateEducationInput = require("../../validation/profile/education");
 const validateExperienceInput = require("../../validation/profile/experience");
-const validateStudentFieldsInput = require("../../validation/profile/studentFields");
-const validateTeacherFieldsInput = require("../../validation/profile/teacherFields");
+const validateCoursesEnrolledInInput = require("../../validation/profile/coursesEnrolledIn");
+const validateCoursesTeachingInput = require("../../validation/profile/coursesTeaching");
 
 /*
  * @route   GET /api/v1/profile/test
@@ -50,7 +50,7 @@ router.get(
 );
 
 /*
- * @route   POST /api/profile
+ * @route   POST /api/v1/profile
  * @params  {handle, bio, skills, youtube, twitter, linkedin, facebook, instagram, github, isStudent, studentId, teacherId}
  * @desc    Create Or Edit User Profile
  * @access  Private
@@ -78,7 +78,9 @@ router.post(
             errors.handle = "That handle already exists";
             res.status(400).json(errors);
           }
-          new Profile(body).save().then(profile => res.json(profile));
+          new Profile(body)
+            .save()
+            .then(profile => res.status(201).json(profile));
         });
       }
     });
@@ -86,7 +88,7 @@ router.post(
 );
 
 /*
- * @route       GET /api/profile/handle/:handle
+ * @route       GET /api/v1/profile/handle/:handle
  * @params URL  {handle}
  * @desc        Get Profile By Handle
  * @access      Public
@@ -110,7 +112,7 @@ router.get("/handle/:handle", (req, res) => {
 });
 
 /*
- * @route       GET /api/profile/user/:user_id
+ * @route       GET /api/v1/profile/user/:user_id
  * @params URL  {user_id}
  * @desc        Get Profile By User ID
  * @access      Public
@@ -134,9 +136,9 @@ router.get("/user/:user_id", (req, res) => {
 });
 
 /*
- * @route   POST /api/profile/education
+ * @route   POST /api/v1/profile/education
  * @params  {school, location, degree, fieldOfStudy, from, to, isCurrent, description}
- * @desc    Add Education To Profile
+ * @desc    Add Education Field To Profile
  * @access  Private
  */
 router.post(
@@ -152,15 +154,15 @@ router.post(
     Profile.findOne({ user: req.user.id }).then(profile => {
       const newEducation = body;
       profile.education.unshift(newEducation);
-      profile.save().then(profile => res.json(profile));
+      profile.save().then(profile => res.status(201).json(profile));
     });
   }
 );
 
 /*
- * @route   POST /api/profile/experience
+ * @route   POST /api/v1/profile/experience
  * @params  {title, company, location, from, to, isCurrent, description}
- * @desc    Add Experience To Profile
+ * @desc    Add Experience Field To Profile
  * @access  Private
  */
 router.post(
@@ -176,63 +178,63 @@ router.post(
     Profile.findOne({ user: req.user.id }).then(profile => {
       const newExperience = body;
       profile.experience.unshift(newExperience);
-      profile.save().then(profile => res.json(profile));
+      profile.save().then(profile => res.status(201).json(profile));
     });
   }
 );
 
 /*
- * @route   POST /api/profile/studentField
+ * @route   POST /api/v1/profile/coursesEnrolledIn
  * @params  {name, type, number, firstName, lastName}
- * @desc    Add Student Field To Profile
+ * @desc    Add coursesEnrolledIn Field [Student] To Profile
  * @access  Private
  */
 router.post(
-  "/studentField",
+  "/coursesEnrolledIn",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const { errors, body, isValid } = validateStudentFieldsInput(req.body);
+    const { errors, body, isValid } = validateCoursesEnrolledInInput(req.body);
 
     if (!isValid) {
       return res.status(400).json(errors);
     }
 
     Profile.findOne({ user: req.user.id }).then(profile => {
-      const newStudentField = body;
-      profile.studentFields.coursesEnrolledIn.unshift(newStudentField);
-      profile.save().then(profile => res.json(profile));
+      const newCoursesEnrolledIn = body;
+      profile.studentFields.coursesEnrolledIn.unshift(newCoursesEnrolledIn);
+      profile.save().then(profile => res.status(201).json(profile));
     });
   }
 );
 
 /*
- * @route   POST /api/profile/teacherField
+ * @route   POST /api/v1/profile/coursesTeaching
  * @params  {name, type, number, firstName, lastName, studentId}
- * @desc    Add Teacher Field To Profile
+ * @desc    Add coursesTeaching Field [Teacher] To Profile
  * @access  Private
  */
 router.post(
-  "/teacherField",
+  "/coursesTeaching",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const { errors, body, isValid } = validateTeacherFieldsInput(req.body);
+    const { errors, body, isValid } = validateCoursesTeachingInput(req.body);
 
     if (!isValid) {
       return res.status(400).json(errors);
     }
 
     Profile.findOne({ user: req.user.id }).then(profile => {
-      const newTeacherField = body;
-      profile.teacherFields.coursesTeaching.unshift(newTeacherField);
-      profile.save().then(profile => res.json(profile));
+      const newCoursesTeaching = body;
+      profile.teacherFields.coursesTeaching.unshift(newCoursesTeaching);
+      profile.save().then(profile => res.status(201).json(profile));
     });
   }
 );
 
 /*
- * @route       DELETE /api/profile/education/:edu_id
+ * @route       DELETE /api/v1/profile/education/:edu_id
  * @params URL  {edu_id}
- * @desc        Delete Education From Profile
+ * @desc        Delete Education Field From Profile
  * @access      Private
  */
 router.delete(
@@ -256,9 +258,9 @@ router.delete(
 );
 
 /*
- * @route       DELETE /api/profile/experience/:exp_id
+ * @route       DELETE /api/v1/profile/experience/:exp_id
  * @params URL  {exp_id}
- * @desc        Delete Experience From Profile
+ * @desc        Delete Experience Field From Profile
  * @access      Private
  */
 router.delete(
@@ -282,13 +284,13 @@ router.delete(
 );
 
 /*
- * @route       DELETE /api/profile/studentField/:studentField_id
- * @params URL  {studentField_id}
- * @desc        Delete Student Field From Profile
+ * @route       DELETE /api/v1/profile/studentField/:coursesEnrolledIn_id
+ * @params URL  {coursesEnrolledIn_id}
+ * @desc        Delete coursesEnrolledIn Field From Profile
  * @access      Private
  */
 router.delete(
-  "/studentField/:studentField_id",
+  "/coursesEnrolledIn/:coursesEnrolledIn_id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
@@ -296,25 +298,25 @@ router.delete(
     Profile.findOne({ user: req.user.id })
       .then(profile => {
         profile.studentFields.coursesEnrolledIn = profile.studentFields.coursesEnrolledIn.filter(
-          item => item.id != req.params.studentField_id
+          item => item.id != req.params.coursesEnrolledIn_id
         );
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => {
-        errors.exp = `Could Not Remove Sudent Field By Id: ${err}`;
+        errors.exp = `Could Not Remove coursesEnrolledIn Field By Id: ${err}`;
         res.status(404).json(errors);
       });
   }
 );
 
 /*
- * @route       DELETE /api/profile/teacherField/:teacherField_id
- * @params URL  {teacherField_id}
- * @desc        Delete Teacher Field From Profile
+ * @route       DELETE /api/v1/profile/teacherField/:coursesTeaching_id
+ * @params URL  {coursesTeaching_id}
+ * @desc        Delete coursesTeaching Field From Profile
  * @access      Private
  */
 router.delete(
-  "/teacherField/:teacherField_id",
+  "/coursesTeaching/:coursesTeaching_id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
@@ -322,19 +324,19 @@ router.delete(
     Profile.findOne({ user: req.user.id })
       .then(profile => {
         profile.teacherFields.coursesTeaching = profile.teacherFields.coursesTeaching.filter(
-          item => item.id != req.params.teacherField_id
+          item => item.id != req.params.coursesTeaching_id
         );
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => {
-        errors.exp = `Could Not Remove Teacher Field By Id: ${err}`;
+        errors.exp = `Could Not Remove coursesTeaching Field By Id: ${err}`;
         res.status(404).json(errors);
       });
   }
 );
 
 /*
- * @route       DELETE /api/profile/:profile_id
+ * @route       DELETE /api/v1/profile/:profile_id
  * @params URL  {user_id}
  * @desc        Delete Profile By Profile Id
  * @access      Private
@@ -354,7 +356,7 @@ router.delete(
 );
 
 /*
- * @route   DELETE /api/profile
+ * @route   DELETE /api/v1/profile
  * @desc    Delete User And Profile
  * @access  Private
  */
--- server/v1/tests/profile.test.js
+++ server/v1/tests/profile.test.js
@@ -2,13 +2,96 @@ const host = process.env.HOST || `127.0.0.1`;
 const port = process.env.PORT || 3001;
 const request = require("supertest").agent(`${host}:${port}`);
 
+let token = null;
+let user_id = "";
+let edu_id = "";
+let exp_id = "";
+let coursesEnrolledIn_id = "";
+let coursesTeaching_id = "";
+
+/*
+ * @route   POST /api/v1/users/register
+ * @params  {firstName, lastName, email, password, password2}
+ * @desc    Register User
+ * @access  Public
+ */
+describe("POST /api/v1/users/register", () => {
+  const body = {
+    firstName: "Taylor",
+    lastName: "Swift",
+    email: "TSwift@Nyit.edu",
+    password: "123456",
+    password2: "123456"
+  };
+  it("Should Respond With 201 Created [User Created]", done => {
+    request
+      .post("/api/v1/users/register")
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+/*
+ * @route   POST /api/v1/users/login
+ * @params  {email, password}
+ * @desc    Logs In User, Save JWT Token
+ * @access  Public
+ */
+describe("POST /api/v1/users/login", () => {
+  const body = {
+    email: "TSwift@Nyit.edu",
+    password: "123456"
+  };
+  it("Should Respond With 200 OK [JWT Token Set]", done => {
+    request
+      .post("/api/v1/users/login")
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        else {
+          token = res.body.token;
+        }
+        done();
+      });
+  });
+});
+
+/*
+ * @route          GET /api/v1/users/current
+ * @authorization  {token}
+ * @desc           Test for current route, should return 200 OK and JSON
+ * @access         Private
+ */
+describe("GET /api/v1/users/current", () => {
+  it("Should Return 200 OK [User_Id Set]", done => {
+    request
+      .get("/api/v1/users/current")
+      .set("Authorization", token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        else user_id = res.body.id;
+        done();
+      });
+  });
+});
+
+// Profile Testing
+
 /*
  * @route   GET /api/v1/profile/test
- * @desc    Tests profile route, should respond with JSON message and 200 OK
+ * @desc    Tests Profile Route, Simple Check If Server Is Running
  * @access  Public
  */
 describe("GET /api/v1/profile/test", () => {
-  it("Should Return JSON Message and 200 OK", done => {
+  it("Should Return 200 OK [Server Is Running]", done => {
     request
       .get("/api/v1/profile/test")
       .set("Accept", "application/json")
@@ -19,5 +102,400 @@ describe("GET /api/v1/profile/test", () => {
         },
         done
       );
+  });
+});
+
+/*
+ * @route   GET /api/v1/profile
+ * @desc    Return Current Users Profile
+ * @access  Private
+ */
+describe("GET /api/v1/profile/", () => {
+  it("Should Return 404 [There is no profile for this user]", done => {
+    request
+      .get("/api/v1/profile")
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(
+        404,
+        {
+          noProfile: "There is no profile for this user"
+        },
+        done
+      );
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile
+ * @params  {handle, bio, skills, youtube, twitter, linkedin, facebook, instagram, github, isStudent, studentId, teacherId}
+ * @desc    Create User Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile", () => {
+  const body = {
+    handle: "Taylor Swift",
+    bio: "I am currently an undergraduate art student in my senior year at NYU",
+    skills: "Java,Python,Javascript,Public Speaking,AutoCAD,Accounting",
+    youtube: "http://youtube.com/TaylorSwift",
+    twitter: "http://twitter.com/TaylorSwift",
+    linkedin: "http://linkedin.com/in/TaylorSwift",
+    facebook: "http://facebook.com/TaylorSwift",
+    instagram: "http://instagram.com/TaylorSwift",
+    github: "http://github.com/TaylorSwift",
+    isStudent: true,
+    studentId: 654321
+  };
+  it("Should Respond With 201 Created [Profile Created]", done => {
+    request
+      .post("/api/v1/profile")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile
+ * @params  {handle, bio, skills, youtube, twitter, linkedin, facebook, instagram, github, isStudent, studentId, teacherId}
+ * @desc    Edit User Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile", () => {
+  const body = {
+    handle: "Taylor Swift",
+    bio:
+      "I am currently an undergraduate art student in my senior year at NYU UPDATED",
+    skills: "Java,Python,Javascript,Public Speaking,AutoCAD,Accounting",
+    youtube: "http://youtube.com/TaylorSwift",
+    twitter: "http://twitter.com/TaylorSwift",
+    linkedin: "http://linkedin.com/in/TaylorSwift",
+    facebook: "http://facebook.com/TaylorSwift",
+    instagram: "http://instagram.com/TaylorSwift",
+    github: "http://github.com/TaylorSwift",
+    isStudent: true,
+    studentId: 123456
+  };
+  it("Should Respond With 200 OK [Profile Updated]", done => {
+    request
+      .post("/api/v1/profile")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(200)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+/*
+ * @route       GET /api/v1/profile/handle/:handle
+ * @params URL  {handle}
+ * @desc        Get Profile By Handle
+ * @access      Public
+ */
+describe("GET /api/v1/profile/handle/:handle", () => {
+  it("Should Return 200 OK [User Retrieved By Handle]", done => {
+    request
+      .get("/api/v1/profile/handle/TaylorSwift")
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route       GET /api/v1/profile/handle/:handle
+ * @params URL  {handle}
+ * @desc        Get Profile By Handle
+ * @access      Public
+ */
+describe("GET /api/v1/profile/handle/:handle", () => {
+  it("Should Return 404 [Could Not Return Profile By Handle]", done => {
+    request
+      .get("/api/v1/profile/handle/JohnDoe")
+      .set("Accept", "application/json")
+      .expect(404, done);
+  });
+});
+
+/*
+ * @route       GET /api/v1/profile/user/:user_id
+ * @params URL  {user_id}
+ * @desc        Get Profile By User ID
+ * @access      Public
+ */
+describe("GET /api/v1/profile/user/:user_id", () => {
+  it("Should Return 200 OK [User Retrieved By User Id]", done => {
+    request
+      .get(`/api/v1/profile/user/${user_id}`)
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route       GET /api/v1/profile/user/:user_id
+ * @params URL  {user_id}
+ * @desc        Get Profile By User ID
+ * @access      Public
+ */
+describe("GET /api/v1/profile/user/:user_id", () => {
+  it("Should Return 404 [There is no profile for this user]", done => {
+    request
+      .get(`/api/v1/profile/user/2384298347`)
+      .set("Accept", "application/json")
+      .expect(404, done);
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile/education
+ * @params  {school, location, degree, fieldOfStudy, from, to, isCurrent, description}
+ * @desc    Add Education To Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile/education", () => {
+  const body = {
+    school: "New York University",
+    location: "New York City",
+    degree: "B.A Science",
+    fieldOfStudy: "Art",
+    from: "2015-09-01",
+    to: "",
+    isCurrent: true,
+    description: "Obtaining My Associates Degree Here"
+  };
+  it("Should Respond With 201 Created [Education Added To Profile]", done => {
+    request
+      .post("/api/v1/profile/education")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        else edu_id = res.body.id;
+        done();
+      });
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile/experience
+ * @params  {title, company, location, from, to, isCurrent, description}
+ * @desc    Add Experience To Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile/experience", () => {
+  const body = {
+    title: "Department Manager",
+    company: "Museum Of Modern Art",
+    location: "321 East Park Ave, New York City",
+    from: "2014-09-01",
+    to: "2015-08-01",
+    isCurrent: false,
+    description: "Did <Insert Manager Duties!> Here"
+  };
+  it("Should Respond With 201 Created [Experience Added To Profile]", done => {
+    request
+      .post("/api/v1/profile/experience")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        else exp_id = res.body.id;
+        done();
+      });
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile/coursesEnrolledIn
+ * @params  {name, type, number, firstName, lastName}
+ * @desc    Add coursesEnrolledIn [Student] To Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile/coursesEnrolledIn", () => {
+  const body = {
+    name: "Operating Systems Security",
+    type: "CSCI",
+    number: 330,
+    firstName: "Howard",
+    lastName: "CLutz"
+  };
+  it("Should Respond With 201 Created [coursesEnrolledIn Added To Profile]", done => {
+    request
+      .post("/api/v1/profile/coursesEnrolledIn")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        else coursesEnrolledIn_id = res.body.id;
+        done();
+      });
+  });
+});
+
+/*
+ * @route   POST /api/v1/profile/coursesTeaching
+ * @params  {name, type, number, firstName, lastName, studentId}
+ * @desc    Add coursesTeaching [Teacher] To Profile
+ * @access  Private
+ */
+describe("POST /api/v1/profile/coursesTeaching", () => {
+  const body = {
+    name: "Operating Systems Security",
+    type: "CSCI",
+    number: 330,
+    firstName: "Taylor",
+    lastName: "Swift",
+    studentId: 654321
+  };
+  it("Should Respond With 201 Created [coursesTeaching Added To Profile]", done => {
+    request
+      .post("/api/v1/profile/coursesTeaching")
+      .set("Authorization", token)
+      .send(body)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        else coursesTeaching_id = res.body.id;
+        done();
+      });
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/education/:edu_id
+ * @params URL  {edu_id}
+ * @desc        Delete Education From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/education/:edu_id", () => {
+  it("Should Return 200 OK [Education Deleted From Profile]", done => {
+    request
+      .delete(`/api/v1/profile/education/${edu_id}`)
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/education/:edu_id
+ * @params URL  {edu_id}
+ * @desc        Delete Education From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/education/:edu_id", () => {
+  it("Should Return 401 Unauthorized [Not Authenticated]", done => {
+    request
+      .delete(`/api/v1/profile/education/${edu_id}`)
+      .set("Accept", "application/json")
+      .expect(401, done);
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/experience/:exp_id
+ * @params URL  {exp_id}
+ * @desc        Delete Experience From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/experience/:exp_id", () => {
+  it("Should Return 200 OK [Experience Deleted From Profile]", done => {
+    request
+      .delete(`/api/v1/profile/experience/${exp_id}`)
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/experience/:exp_id
+ * @params URL  {exp_id}
+ * @desc        Delete Experience From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/experience/:exp_id", () => {
+  it("Should Return 401 Unauthorized [Not Authenticated]", done => {
+    request
+      .delete(`/api/v1/profile/experience/${exp_id}`)
+      .set("Accept", "application/json")
+      .expect(401, done);
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/coursesEnrolledIn/:coursesEnrolledIn_id
+ * @params URL  {coursesEnrolledIn_id}
+ * @desc        Delete coursesEnrolledIn Field From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/coursesEnrolledIn/:coursesEnrolledIn_id", () => {
+  it("Should Return 200 OK [CoursesEnrolledIn Field Deleted From Profile]", done => {
+    request
+      .delete(`/api/v1/profile/coursesEnrolledIn/${coursesEnrolledIn_id}`)
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route       DELETE /api/v1/profile/teacherField/:coursesTeaching_id
+ * @params URL  {coursesTeaching_id}
+ * @desc        Delete coursesTeaching Field From Profile
+ * @access      Private
+ */
+describe("DELETE /api/v1/profile/coursesTeaching/:coursesTeaching_id", () => {
+  it("Should Return 200 OK [coursesTeaching Field Deleted From Profile]", done => {
+    request
+      .delete(`/api/v1/profile/coursesTeaching/${coursesTeaching_id}`)
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
+
+/*
+ * @route   DELETE /api/v1/profile
+ * @desc    Delete User And Profile
+ * @access  Private
+ */
+describe("DELETE /api/v1/profile", () => {
+  it("Should Return 401 Unauthorized [No JWT Token]", done => {
+    request
+      .delete("/api/v1/profile")
+      .set("Accept", "application/json")
+      .expect(401, done);
+  });
+});
+
+/*
+ * @route   DELETE /api/v1/profile
+ * @desc    Delete User And Profile
+ * @access  Private
+ */
+describe("DELETE /api/v1/profile", () => {
+  it("Should Return 200 OK [Authenticated User Deletes Own Profile/User]", done => {
+    request
+      .delete("/api/v1/profile")
+      .set("Authorization", token)
+      .set("Accept", "application/json")
+      .expect(200, done);
   });
 });
