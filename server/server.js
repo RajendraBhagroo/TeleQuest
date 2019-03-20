d0d985567c760c98aa21a600f5b727f4cd739985
Christopher Guevarra <jacob_guevarra2001@yahoo.com>
2019-03-20 17:08:45 -0400

    Added -socket.io server-side -socket.io-streaming Currently functionallity includes -Joining specified route and room in route -Creating specified route, and room in route -Disconnecting from connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cors = require("cors");
-
 const app = express();
+const socket = require("socket.io")(server);
+const server=require('http').Server(app);
+const ss = require('socket.io-stream');
+const connections=[];
 require(`./${process.env.SERVER_VERSION}/passport`)(passport);
 
 // Routes
@@ -31,3 +34,45 @@ app.use(`/api/${process.env.SERVER_VERSION}/profile`, profile);
 const host = process.env.HOST || `127.0.0.1`;
 const port = process.env.NODE_PORT || 3001;
 app.listen(port, () => console.log(`Server running on http://${host}:${port}`));
+
+//Establishing Socket.io server
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+const io=socket(server);
+const roomID = "";
+const nameSpace="";
+
+io
+    .of(`/${nameSpace}`)
+    /* @route   http://host:port/nameSpace
+     * @params  {nameSpace}
+     * @desc    opens up a websocket for the specified route
+     */
+  
+    .on("connection",function(socket){
+      connections.push(socket);
+      console.log(`Connected to ${nameSpace}, number of connections = ${connections.length}`);
+      socket.emit(`Welcome to ${nameSpace}`);
+    
+    /* @route http://host:port/nameSpace/room
+     * @params{room}
+     * @desc joins or creates a room within the given nameSpace
+     */
+    socket.on("joinRoom",function(room){
+      socket.join(room);
+      io.emit(`New User has joined ${room}`);
+    })
+    /* @route 
+     * @params
+     * @desc disconnects user and returns them to the default room in the nameSpace
+     */
+    socket.on("disconnect",function(data){
+      connections.splice(connections.indexOf(socket),1);
+      console.log(`Connection disconnected, number of connections ${connections.length}`);
+      socket.join("/");
+    })
+    })
+
+
+;
