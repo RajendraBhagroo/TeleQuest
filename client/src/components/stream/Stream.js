235614ef5126eabf762d1bfc10297bad9fa71026
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-04-29 14:44:54 -0400

    Reviewed Code For Streams

--- client/src/components/stream/Stream.js
+++ client/src/components/stream/Stream.js
@@ -1,62 +1,53 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../redux/actions/profileActions";
 import ProfileSideCourseCard from "../profile/ProfileSideCourseCard";
 import Spinner from "../common/Spinner";
 import PropTypes from "prop-types";
 import io from "socket.io-client";
-import { Link } from "react-router-dom";
+
 const host = process.env.HOST || `127.0.0.1`;
 const socket_port = process.env.SOCKET_PORT || 3002;
-let namespace="NYIT";
-/*main url being where the website is being hosted as well as the specified porition of that website
-* since rooms are virtuilized in the namespace the url won't change
-*/
-const mainURL=`http://${host}:${socket_port}/`+namespace;
-let socket=io.connect(mainURL);
+let namespace = "NYIT";
 
-//occurs in the event of a err response from the server
-socket.on('err',function(data){
+/*
+ * main url being where the website is being hosted as well as the specified porition of that website
+ * since rooms are virtuilized in the namespace the url won't change
+ */
+const mainURL = `http://${host}:${socket_port}/${namespace}`;
+let socket = io.connect(mainURL);
+
+socket.on("err", function(data) {
   console.log(data);
 });
 
-//@desc occurs in the event of a success
-socket.on('success',function(data){
+socket.on("success", function(data) {
   console.log(data);
 });
 
-//@desc occurs in the event of a response
-socket.on('response',function(data){
+socket.on("response", function(data) {
   console.log(data);
 });
-socket.on('streaming',function(data){
+
+socket.on("streaming", function(data) {
   const video = document.querySelector("streamingVid");
-
 });
 
-//function to get user media 
-function startVideo(){
-    const constraints = { audio: true, video: { width: 400, height: 300 } };
-    let video=document.getElementById("videoElement");
+function startVideo() {
+  const constraints = { audio: true, video: { width: 400, height: 300 } };
+  let video = document.getElementById("videoElement");
 
-    navigator.mediaDevices.getUserMedia(constraints)
+  navigator.mediaDevices
+    .getUserMedia(constraints)
     .then(function(stream) {
-      try{
-      video.srcObject= stream;
-      console.log("Entering try");
-      }
-      catch(e)
-      {
-        console.log("Entering catch");
-        console.log(`Error: ${e}`)
-      }
+      video.srcObject = stream;
       video.play();
-
     })
     .catch(function(err) {
-      console.log("An error occurred: " + err);
+      throw Error("An error occurred: " + err);
     });
-};
+}
 
 class Stream extends Component {
   constructor() {
@@ -66,32 +57,32 @@ class Stream extends Component {
     };
   }
 
-  /*function that is called when a teacher clicks on a classroom
-  * using the clicked on class creates a room
-  * the streamingURL is a combination of the host, socketport
-  * and the name of the class that was clicked
-  */
-   CreateClassRoom(room){
-    socket.emit('newClass',room);
+  /*
+   * Function that is called when a teacher clicks on a classroom
+   * using the clicked on class creates a room
+   * the streamingURL is a combination of the host, socketport
+   * and the name of the class that was clicked
+   */
+  CreateClassRoom(room) {
+    socket.emit("newClass", room);
   }
 
-  /*Function thats called when a student clicks on a classroom that exists
-  * joins the specified room that the user clicked on
-  */
-  JoinClassRoom(room){
-    socket.emit('joinRoom',room);
+  /*
+   * Function thats called when a student clicks on a classroom that exists
+   * joins the specified room that the user clicked on
+   */
+  JoinClassRoom(room) {
+    socket.emit("joinRoom", room);
   }
 
-  /*@desc returns the current avaiable rooms/classes in the namespace
-  */
-  GetRooms(){
-    socket.emit('currentClasses');
+  // returns the current avaiable rooms/classes in the namespace
+  GetRooms() {
+    socket.emit("currentClasses");
   }
 
-  /*@desc emits the start of the stream to those in the room
-  */
-  StartStream(data){
-    socket.emit('stream',data);
+  // emits the start of the stream to those in the room
+  StartStream(data) {
+    socket.emit("stream", data);
   }
 
   componentDidMount() {
@@ -102,12 +93,11 @@ class Stream extends Component {
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
     let publicProfile;
+
     if (profile === null || loading) {
-      console.log("Either profile null or loading")
-      return publicProfile =<Spinner />;
+      return (publicProfile = <Spinner />);
     } else {
       if (profile.handle === undefined) {
-        console.log("profile.handle prob null")
         publicProfile = (
           <div>
             <center>
@@ -125,41 +115,47 @@ class Stream extends Component {
             </center>
           </div>
         );
-      }
-      else{
-        console.log("loading vid player");
-      return (
-        <div>
-          <div className="container">
-            <p>{user.userName}</p>
-            <div className="card">
-              <div className="card-header">
-                <strong>
-                  {profile.isStudent
-                    ? "Courses Enrolled In"
-                    : "Courses Teaching"}
-                </strong>
-              </div>
-              <div>
-                {profile.isStudent ? (
-                  <ProfileSideCourseCard
-                    course={profile.studentFields.coursesEnrolledIn}
-                  />
-                ) : (
-                  <ProfileSideCourseCard
-                    course={profile.teacherFields.coursesTeaching}
-                  />
-                )}
+      } else {
+        return (
+          <div>
+            <div className="container">
+              <p>{user.userName}</p>
+              <div className="card">
+                <div className="card-header">
+                  <strong>
+                    {profile.isStudent
+                      ? "Courses Enrolled In"
+                      : "Courses Teaching"}
+                  </strong>
+                </div>
+                <div>
+                  {profile.isStudent ? (
+                    <ProfileSideCourseCard
+                      course={profile.studentFields.coursesEnrolledIn}
+                    />
+                  ) : (
+                    <ProfileSideCourseCard
+                      course={profile.teacherFields.coursesTeaching}
+                    />
+                  )}
+                </div>
               </div>
             </div>
-                </div>
-                <video id="videoElement">No Video Stream.....</video>
-                 <video id="streamingVid"/>
-                <button id="startStream" onClick={function(){startVideo()}}>Start</button>
-        </div>
-      );
+            <video id="videoElement">No Video Stream.....</video>
+            <video id="streamingVid" />
+            <button
+              id="startStream"
+              onClick={function() {
+                startVideo();
+              }}
+            >
+              Start
+            </button>
+          </div>
+        );
+      }
     }
-  }}
+  }
 }
 
 Stream.propTypes = {
--- server/server.js
+++ server/server.js
@@ -4,14 +4,17 @@ const passport = require("passport");
 const cors = require("cors");
 const app = express();
 require(`./${process.env.SERVER_VERSION}/passport`)(passport);
-let server = require('http').Server(app);
-let io = require('socket.io').listen(server)
-const connections=[];
-let availablerooms=[];
-let currentRoom="";
+
+let server = require("http").Server(app);
+let io = require("socket.io").listen(server);
+const connections = [];
+let availablerooms = [];
+let currentRoom = "";
+
 // Routes
 const users = require(`./${process.env.SERVER_VERSION}/routes/api/users`);
 const profile = require(`./${process.env.SERVER_VERSION}/routes/api/profile`);
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,71 +32,83 @@ mongoose
 app.use(`/api/${process.env.SERVER_VERSION}/users`, users);
 app.use(`/api/${process.env.SERVER_VERSION}/profile`, profile);
 
-// Run Server
+// Run Servers
 const host = process.env.HOST || `127.0.0.1`;
 const socket_port = process.env.SOCKET_PORT || 3002;
-const node_port=process.env.NODE_PORT||3001
-app.listen(node_port,()=>console.log(`Node Server listening on http://${host}:${node_port}`));
-server.listen(socket_port, () => console.log(`Socket.io Server running on http://${host}:${socket_port}`));
-//Establishing Socket.io server
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+const node_port = process.env.NODE_PORT || 3001;
+
+app.listen(node_port, () =>
+  console.log(`Node Server listening on http://${host}:${node_port}`)
+);
+
+server.listen(socket_port, () =>
+  console.log(`Socket.io Server running on http://${host}:${socket_port}`)
+);
+
+// Establishing Socket.io server
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
-const nameSpace="NYIT";
-    /* @route   http://host:port/nameSpace
-     * @params  {nameSpace}
-     * @desc    opens up a socket.io socket for the specified route
-     */
-io
-    .of(`/${nameSpace}`)
-  
-    .on("connection",function(socket){
-      connections.push(socket);
-      console.log(`Connected to ${nameSpace}, number of connections = ${connections.length}`);
-      socket.emit(`Welcome to ${nameSpace}`);
-    
-    /* @route http://host:port/nameSpace/room
-     * @params{room}
-     * @desc joins or creates a room within the given nameSpace
-     */
-    socket.on("joinRoom",function(room){
-      if(availablerooms.includes(room))
-      {
+
+const nameSpace = "NYIT";
+
+/* @route   http://host:port/nameSpace
+ * @params  {nameSpace}
+ * @desc    opens up a socket.io socket for the specified route
+ */
+io.of(`/${nameSpace}`).on("connection", function(socket) {
+  connections.push(socket);
+  console.log(
+    `Connected to ${nameSpace}, number of connections = ${connections.length}`
+  );
+  socket.emit(`Welcome to ${nameSpace}`);
+
+  /* @route http://host:port/nameSpace/room
+   * @params{room}
+   * @desc joins or creates a room within the given nameSpace
+   */
+  socket.on("joinRoom", function(room) {
+    if (availablerooms.includes(room)) {
       socket.join(room);
-      currentRoom=room;
-      return io.emit("success",`New User has joined ${room}`);
-      }
-      else{return socket.emit('err',"This room doesn't exist yet")}
-    });
+      currentRoom = room;
+      return io.emit("success", `New User has joined ${room}`);
+    } else {
+      return socket.emit("err", "This room doesn't exist yet");
+    }
+  });
 
-    /*@desc upon streaming, broadcasts stream to those in the room
-    */
-    socket.on("stream",function(data){
-      io.of(`/${nameSpace}`)
-        .in(currentRoom).emit("streaming",data)
-    });
+  /*@desc upon streaming, broadcasts stream to those in the room
+   */
+  socket.on("stream", function(data) {
+    io.of(`/${nameSpace}`)
+      .in(currentRoom)
+      .emit("streaming", data);
+  });
 
-    /*@desc on event new-class, socket will append the new class to 
-    * availablerooms then joins it
-    */
-    socket.on("newClass",function(data){
-      availablerooms.push(data);
-      socket.join(data);
-    });
-    /*@desc on event currentClasses, socket emits the response of current available rooms
-    * in the namespace
-    */
-    socket.on("currentClasses",function(){
-      return socket.emit('response',availablerooms);
-    })
-    /* @route 
-     * @params
-     * @desc disconnects user and returns them to the default room in the nameSpace
-     */
-    socket.on("disconnect",function(data){
-      connections.splice(connections.indexOf(socket),1);
-      console.log(`Connection disconnected, number of connections ${connections.length}`);
-      socket.join("/");
-    })
-    })
-;
+  /*@desc on event new-class, socket will append the new class to
+   * availablerooms then joins it
+   */
+  socket.on("newClass", function(data) {
+    availablerooms.push(data);
+    socket.join(data);
+  });
+
+  /*@desc on event currentClasses, socket emits the response of current available rooms
+   * in the namespace
+   */
+  socket.on("currentClasses", function() {
+    return socket.emit("response", availablerooms);
+  });
+
+  /* @route
+   * @params
+   * @desc disconnects user and returns them to the default room in the nameSpace
+   */
+  socket.on("disconnect", function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(
+      `Connection disconnected, number of connections ${connections.length}`
+    );
+    socket.join("/");
+  });
+});
