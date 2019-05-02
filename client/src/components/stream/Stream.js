ccc39c76ea067413e5f2cabd8e9be438ce128cc4
Rajendra Bhagroo <RajendraBhagroo@Gmail.com>
2019-05-01 23:26:57 -0400

    Refactored Stream Functions Into Seperate File

--- client/src/components/stream/Stream.js
+++ client/src/components/stream/Stream.js
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../redux/actions/profileActions";
 import ProfileSideCourseCard from "../profile/ProfileSideCourseCard";
+import {
+  startVideo,
+  startStream,
+  stopStream,
+  startRecording,
+  endRecording
+} from "./StreamFunctions";
 import Spinner from "../common/Spinner";
 import PropTypes from "prop-types";
-import io from "socket.io-client";
 
 const styles = {
   Card: {
@@ -43,206 +49,6 @@ const styles = {
   }
 };
 
-const host = process.env.HOST || `127.0.0.1`;
-const socket_port = process.env.SOCKET_PORT || 3002;
-let namespace = "NYIT";
-var outBoundStream;
-let peer1 = new RTCPeerConnection();
-let peer2 = new RTCPeerConnection();
-let streaming = false;
-let avaiableCourse = [];
-let mediaRecorder;
-let chunks = [];
-
-/*
- * main url being where the website is being hosted as well as the specified porition of that website
- * since rooms are virtuilized in the namespace the url won't change
- */
-const mainURL = `http://${host}:${socket_port}/${namespace}`;
-let socket = io.connect(mainURL);
-
-socket.on("err", data => {
-  console.log(data);
-});
-
-socket.on("success", data => {
-  console.log(data);
-});
-
-socket.on("response", data => {
-  console.log(data);
-});
-
-socket.on("response_offer", async data => {
-  console.log("Recieved Data Offer:");
-  console.log(data);
-  await peer2
-    .setRemoteDescription(data)
-    .then(() => peer2.createAnswer())
-    .then(sdp => peer2.setLocalDescription(sdp))
-    .then(() => {
-      socket.emit("answer", peer2.localDescription);
-    });
-});
-
-socket.on("remote_answer", async data => {
-  console.log("Recieved Data Answer:");
-  console.log(data);
-  await peer1.setRemoteDescription(data);
-});
-
-socket.on("avaiableCourse", data => {
-  avaiableCourse = data;
-});
-
-let startVideo = () => {
-  const constraints = { audio: true, video: { width: 400, height: 300 } };
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(stream => {
-      streaming = true;
-      let video = document.getElementById("videoElement");
-      let startRecording = document.getElementById("startRecord");
-      video.srcObject = stream;
-      outBoundStream = stream;
-      video.play();
-    })
-    .catch(err => {
-      throw Error("An error occurred: " + err);
-    });
-};
-
-let startRecording = () => {
-  mediaRecorder = new MediaRecorder(outBoundStream);
-  mediaRecorder.start();
-  console.log(mediaRecorder.state);
-  mediaRecorder.ondataavailable = e => {
-    chunks.push(e.data);
-  };
-  mediaRecorder.onStop = e => {
-    let blob = new Blob(chunks, { type: "video/mp4" });
-    let vod = prompt("Enter a name for your stream VOD");
-    chunks = [];
-  };
-};
-
-let endRecording = () => {
-  mediaRecorder.stop();
-  console.log(mediaRecorder.state);
-};
-
-peer1.addEventListener("icecandidate", e => onIceCandidate(peer1, e));
-peer2.addEventListener("icecandidate", e => onIceCandidate(peer2, e));
-peer1.addEventListener("iceconnectionstatechange", e =>
-  onIceStateChange(peer1, e)
-);
-peer2.addEventListener("iceconnectionstatechange", e =>
-  onIceStateChange(peer2, e)
-);
-
-let getName = pc => {
-  return pc === peer1 ? "peer1" : "peer2";
-};
-
-let getOtherPc = pc => {
-  return pc === peer1 ? peer2 : peer1;
-};
-
-let onIceCandidate = async (pc, event) => {
-  try {
-    await getOtherPc(pc).addIceCandidate(event.candidate);
-    onAddIceCandidateSuccess(pc);
-  } catch (e) {
-    onAddIceCandidateError(pc, e);
-  }
-  console.log(
-    `${getName(pc)} ICE candidate:\n${
-      event.candidate ? event.candidate.candidate : "(null)"
-    }`
-  );
-};
-
-let onAddIceCandidateSuccess = pc => {
-  console.log(`${getName(pc)} addIceCandidate success`);
-};
-
-let onAddIceCandidateError = (pc, error) => {
-  console.log(
-    `${getName(pc)} failed to add ICE Candidate: ${error.toString()}`
-  );
-};
-
-let onIceStateChange = (pc, event) => {
-  if (pc) {
-    console.log(`${getName(pc)} ICE state: ${pc.iceConnectionState}`);
-    console.log("ICE state change event: ", event);
-  }
-};
-
-let onCreateSessionDescError = error => {
-  console.log(`Failed to create session description: ${error.toString()}`);
-};
-
-/*
- * Function that is called when a teacher clicks on a classroom
- * using the clicked on class creates a room
- * the streamingURL is a combination of the host, socketport
- * and the name of the class that was clicked
- */
-let CreateClassRoom = room => {
-  socket.emit("newClass", room);
-};
-
-/*
- * Function thats called when a student clicks on a classroom that exists
- * joins the specified room that the user clicked on
- */
-let JoinClassRoom = room => {
-  socket.emit("joinRoom", room);
-};
-
-// returns the current avaiable rooms/classes in the namespace
-let GetRooms = () => {
-  socket.emit("currentClasses");
-};
-
-// emits the start of the stream to those in the room
-let startStream = async data => {
-  CreateClassRoom("Test");
-  if (streaming) {
-    peer2.ontrack = event => {
-      console.log("recieving foriegn stream");
-      let incomeVid = document.getElementById("ForiegnVid");
-      incomeVid.srcObject = event.streams[0];
-      incomeVid.play();
-    };
-    outBoundStream
-      .getTracks()
-      .forEach(track => peer1.addTrack(track, outBoundStream));
-    try {
-      console.log("peer1 createOffer start");
-      await peer1
-        .createOffer()
-        .then(sdp => peer1.setLocalDescription(sdp))
-        .then(() => {
-          console.log("Peer1 LocalDescription:");
-          console.log(peer1.localDescription);
-          socket.emit("offer", peer1.localDescription);
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-};
-
-let stopStream = () => {
-  if (streaming) {
-    console.log(outBoundStream);
-    let tracks = outBoundStream.getTracks();
-
-    tracks.forEach(track => track.stop());
-  }
-};
 class Stream extends Component {
   constructor() {
     super();
@@ -345,6 +151,7 @@ class Stream extends Component {
                     >
                       Start Recording
                     </button>
+
                     <button
                       id="stopRecording"
                       onClick={() => endRecording()}
